e5a53f4ea15f8a2ce2edb66a8ee8aa2a3c382839
anydict <novi4kom@gmail.com>
2023-04-06 02:31:43 +0300

    add Dockerfile_ubuntu

--- docker/docker_run_asterisk.sh
+++ docker/docker_run_asterisk.sh
@@ -4,24 +4,26 @@
 # bash docker_run_asterisk.sh 2 18.1.1
 
 if [ -z "$1" ]; then
-  echo "No instance number. Use 0-9. Example docker_run_asterisk 2" && exit 0
+  instance_name=asterisk_extapi-0
+  # echo "No instance number. Use 0-9. Example docker_run_asterisk 2" && exit 0
 else
   instance_name=asterisk_extapi-$1
 fi
 
 if [ -z "$2" ]; then
-  echo "No version" && exit 0
+  version=20.2.0
+  # echo "No version" && exit 0
 else
   version=$2
 fi
 
-docker stop "$instance_name"
-docker rm "$instance_name"
+/run/host/bin/docker stop "$instance_name"
+/run/host/bin/docker rm "$instance_name"
 
 host_conf_path="/etc/$instance_name" && mkdir -p "/etc/$instance_name"
 host_logs_path="/var/log/$instance_name" && mkdir -p "/var/log/$instance_name"
 
-docker run \
+/run/host/bin/docker run \
   -d \
   --restart=always \
   --net=host \
@@ -29,5 +31,6 @@ docker run \
   -v "$host_conf_path":/etc/asterisk \
   -v /tmp/:/tmp/ \
   --name "$instance_name" \
-  anydict/asterisk:"$version"
+  asterisk20v5:latest
+  #anydict/asterisk:"$version"
 
