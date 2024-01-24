4ae5a0c533397256cdc2a7b2dc8fb3f2e9e0dbdc
anydict <novi4kom@gmail.com>
2024-01-24 13:27:55 +0300

    changed build version asterisk from 20.5.0 to 20.6.0

--- docker/build_asterisk.sh
+++ docker/build_asterisk.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-# Build image Asterisk 20.5.0
+# Build image Asterisk 20.6.0
 
-version=20.5.0
+version=20.6.0
 
 echo Build docker of Asterisk, version: ${version}
 
 docker build -t anydict/asterisk:${version} -f Dockerfile .
 ### For full build use line below and replace line above:
-# -f Dockerfile_source .
+# docker build -t anydict/asterisk:${version} -f Dockerfile_source .
