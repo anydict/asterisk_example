752737b9b34571a2922caff93018c239e3a597d4
anydict <novi4kom@gmail.com>
2024-03-19 02:23:31 +0300

    upgrade to new version Asterisk 20.7.0

--- docker/build_asterisk.sh
+++ docker/build_asterisk.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-# Build image Asterisk 20.6.0
+# Build image Asterisk 20.7.0
 
-version=20.6.0
+version=20.7.0
 
 echo Build docker of Asterisk, version: ${version}
 
-docker build -t anydict/asterisk:${version} -f Dockerfile .
+# docker build -t anydict/asterisk:${version} -f Dockerfile .
 ### For full build use line below and replace line above:
-# docker build -t anydict/asterisk:${version} -f Dockerfile_source .
+docker build -t anydict/asterisk:${version} -f Dockerfile_source .
--- docker/docker_run_asterisk.sh
+++ docker/docker_run_asterisk.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-# Run instance with number 2 with version image Asterisk 20.5.0
-# bash docker_run_asterisk.sh 2 20.5.0
+# Run instance with number 2 with version image Asterisk 20.7.0
+# bash docker_run_asterisk.sh 2 20.7.0
 
 if [ -z "$1" ]; then
   instance_name=asterisk_extapi-0
@@ -11,7 +11,7 @@ else
 fi
 
 if [ -z "$2" ]; then
-  version=20.5.0
+  version=20.7.0
   # echo "No version" && exit 0
 else
   version=$2
